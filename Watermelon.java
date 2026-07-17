49360598db551c7f97b2e38cfca45bda9aa1d8fd
Anvesha Srivastava <207409304+Anvesha-Sri@users.noreply.github.com>
2026-07-17 22:41:01 +0530

    Update Watermelon.java

--- Watermelon.java
+++ Watermelon.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+public class Watermelon {
+    public static void main (String arg[])
+    {
+        Scanner sc = new Scanner (System.in);
+        int w = sc.nextInt();
+        if (w % 2 == 0 && w > 2)
+        {
+            System.out.print("YES");
+            
+        }
+        else {
+            System.out.print("NO");
+        }
+    }
+}
