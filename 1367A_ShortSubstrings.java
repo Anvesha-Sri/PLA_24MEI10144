b8a20d0a5d7cabf87daaac549007cd0130467684
Anvesha Srivastava <207409304+Anvesha-Sri@users.noreply.github.com>
2026-08-25 21:57:56 +0530

    Create 1367A_ShortSubstrings.java

---
+++ 1367A_ShortSubstrings.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+
+            StringBuilder ans = new StringBuilder();
+
+            for (int i = 0; i < s.length() - 1; i += 2) {
+                ans.append(s.charAt(i));
+            }
+
+            ans.append(s.charAt(s.length() - 1));
+
+            System.out.println(ans);
+        }
+    }
+}
