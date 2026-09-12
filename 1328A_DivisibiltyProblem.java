103126205a5fdd7e77d6811277691048a8024e4a
Anvesha Srivastava <207409304+Anvesha-Sri@users.noreply.github.com>
2026-09-12 19:17:35 +0530

    Implement divisibility problem solution

---
+++ 1328A_DivisibiltyProblem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            System.out.println((b - a % b) % b);
+        }
+    }
+}
